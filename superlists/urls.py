4c7bcc8cefb6ee1102900989e9451aad3f2f1b2c
volkan <volkan.kaya@hotmail.co.uk>
2015-06-24 22:05:55 +0100

    added url to lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import include, url
+from lists import views as list_views
+from lists import urls as list_urls
 
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+urlpatterns = [
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
